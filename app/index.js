f3ed76fad2a4bcc0df0a216cb99b30326946a840
douglasbrandao00 <douglaswfbrandao@gmail.com>
2018-08-30 15:36:25 -0300

    parsing query string

--- app/index.js
+++ app/index.js
@@ -6,14 +6,16 @@ const port = 3000
 const server = http.createServer((req, res) => {
   const parseUrl = url.parse(req.url, true)
   
-  const path = url
-    .parse(req.url, true)
+  const path = parseUrl
     .pathname
     .replace(/^\/+|\/+$/g, '')
 
+  const queryParams = parseUrl.query
+  console.log(queryParams)
+
   const method = req.method.toUpperCase()
 
-  console.log(`Request recived on path ${path} on method ${method}`)
+  console.log(`Request on path ${path} on method ${method} on query ${JSON.stringify(queryParams)}`)
 
   res.end('Racionais 4:3\n')
 })
