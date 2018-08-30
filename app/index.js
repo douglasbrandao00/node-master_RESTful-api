05ec558116688696f684395dc6deeabf783082f7
douglasbrandao00 <douglaswfbrandao@gmail.com>
2018-08-30 12:30:21 -0300

    parsing URL

--- app/index.js
+++ app/index.js
@@ -1,8 +1,18 @@
 const http = require('http')
+const url = require('url')
 
 const port = 3000
 
 const server = http.createServer((req, res) => {
+  const parseUrl = url.parse(req.url, true)
+  
+  const path = url
+    .parse(req.url, true)
+    .pathname
+    .replace(/^\/+|\/+$/g, '')
+
+  console.log(`Request recived on path ${path}`)
+
   res.end('Racionais 4:3\n')
 })
 
